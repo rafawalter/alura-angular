e2917cffdc40e7109bd47c31fde80e116434235b
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-09-04 11:27:40 -0300

    03.08 Consumindo dados da API

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -7,18 +7,14 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  photos = [
-    {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
-      description: 'Leão'
-    },
-    {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
-      description: 'Leoa'
-    }
-  ];
+  photos: Object[] = [];
 
   constructor(http: HttpClient) {
-    console.log(http);
+    http
+      .get<Object[]>('http://localhost:3000/flavio/photos')
+      .subscribe(
+        photos => this.photos = photos,
+        err => console.log(err.message)
+      );
   }
 }
