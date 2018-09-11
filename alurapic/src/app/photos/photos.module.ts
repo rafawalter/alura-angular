bb434ac487699896c163ab022dcc3d145d289bdf
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-09-11 12:04:52 -0300

    05.07 Logica do Grid + Change detection

--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -5,6 +5,7 @@ import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { CommonModule } from '@angular/common';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
 
 @NgModule({
     imports: [
@@ -14,7 +15,8 @@ import { PhotoFormComponent } from './photo-form/photo-form.component';
     declarations: [
         PhotoComponent,
         PhotoListComponent,
-        PhotoFormComponent
+        PhotoFormComponent,
+        PhotosComponent
     ],
 })
 export class PhotosModule {
