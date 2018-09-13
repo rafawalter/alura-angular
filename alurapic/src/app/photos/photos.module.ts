1b25d6f802818d8bf2d37e3ad8fcf16e444881b5
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-09-13 11:19:06 -0300

    07.02 Sub-modulos

--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -1,26 +1,14 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { CommonModule } from '@angular/common';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
 
 @NgModule({
     imports: [
-        CommonModule,
-        HttpClientModule,
-    ],
-    declarations: [
-        PhotoComponent,
-        PhotoListComponent,
-        PhotoFormComponent,
-        PhotosComponent,
-        FilterByDescription,
-        LoadButtonComponent
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule,
     ],
 })
 export class PhotosModule {
