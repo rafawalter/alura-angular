1cc791f26b159dbde0bb8b7721346c9a85442a4c
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-09-13 12:48:53 -0300

    localhost:4201

--- alurapic/scripts/docker-serve.sh
+++ alurapic/scripts/docker-serve.sh
@@ -1,2 +1,1 @@
-echo ### http://localhost:4201
-docker run --rm -it --name alura-angular -v "$PWD":/usr/app -w /usr/app -p 4201:4200 -p 49153:49153 rfwal:alura-angular ng serve --host 0.0.0.0 -o
+docker run --rm -it --name alura-angular -v "$PWD":/usr/app -w /usr/app -p 4201:4201 -p 49153:49153 rfwal:alura-angular ng serve --host 0.0.0.0 -o
