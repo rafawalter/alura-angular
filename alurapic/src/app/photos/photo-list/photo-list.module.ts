322765300a8af8d9397503e5f8e6e00565cc22aa
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-09-13 12:23:38 -0300

    07.06 Componentizando o filtro

--- alurapic/src/app/photos/photo-list/photo-list.module.ts
+++ alurapic/src/app/photos/photo-list/photo-list.module.ts
@@ -7,13 +7,15 @@ import { PhotosComponent } from './photos/photos.component';
 import { FilterByDescription } from './filter-by-description.pipe';
 import { PhotoModule } from '../photo/photo.module';
 import { CardModule } from '../../shared/components/card/card.module';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
     declarations: [
         PhotoListComponent,
         PhotosComponent,
         LoadButtonComponent,
-        FilterByDescription
+        FilterByDescription,
+        SearchComponent
     ],
     imports: [
         CommonModule,
