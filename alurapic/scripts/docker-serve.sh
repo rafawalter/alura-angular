e931dc512ef4345812ce97d596fe72a7c81c475b
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-09-17 13:21:45 -0300

    07.09 Criando a primeira diretiva

--- alurapic/scripts/docker-serve.sh
+++ alurapic/scripts/docker-serve.sh
@@ -1,1 +1,1 @@
-docker run --rm -it --name alura-angular -v "$PWD":/usr/app -w /usr/app -p 4201:4201 -p 49153:49153 rfwal:alura-angular ng serve --host 0.0.0.0 -o
+docker run --rm -it --name alura-angular -v "$PWD":/usr/app -w /usr/app -p 4201:4201 -p 49153:49153 rfwal:alura-angular ng serve --host 0.0.0.0 -o --port 4201
---
+++ alurapic/src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -0,0 +1,9 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+    // tslint:disable-next-line:directive-selector
+    selector: '[apDarkenOnHover]'
+})
+export class DarkenOnHoverDirective {
+
+}
