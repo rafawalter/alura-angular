ccf52bad379f81f4b4b58b8d7e1fda376ad7b400
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-10-10 10:29:12 -0300

    01.07 Enviando credenciais para a API

--- alurapic/src/app/home/signin/signin.component.ts
+++ alurapic/src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
     templateUrl: './signin.component.html'
@@ -8,7 +9,8 @@ export class SignInComponent implements OnInit {
     loginForm: FormGroup;
 
     constructor(
-        private formBuilder: FormBuilder
+        private formBuilder: FormBuilder,
+        private authService: AuthService
     ) {}
 
     ngOnInit(): void {
@@ -18,4 +20,19 @@ export class SignInComponent implements OnInit {
         });
     }
 
+    login() {
+
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+
+        this.authService
+            .authenticate(userName, password)
+            .subscribe(
+                () => console.log('autenticado'),
+                err => {
+                    console.log(err);
+                    this.loginForm.reset();
+                }
+            );
+    }
 }
