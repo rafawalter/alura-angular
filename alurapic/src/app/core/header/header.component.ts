7f04a183ca0f63082d5b4d40f12b70f37830a692
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-10-15 10:54:22 -0300

    02.08 Cabecalho da aplicação

---
+++ alurapic/src/app/core/header/header.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // tslint:disable-next-line:component-selector
+    selector: 'ap-header',
+    templateUrl: './header.component.html' })
+export class HeaderComponent { }
