1613fadaea209c71007dfd3959dfde27e8f71075
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-10-23 10:33:54 -0300

    03.07 A implementação do logout

--- alurapic/src/app/core/header/header.component.ts
+++ alurapic/src/app/core/header/header.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { UserService } from '../user/user.service';
 import { Observable } from 'rxjs';
@@ -11,8 +12,16 @@ export class HeaderComponent {
 
     user$: Observable<User>;
 
-    constructor(private userService: UserService) {
+    constructor(
+        private userService: UserService,
+        private router: Router
+    ) {
         this.user$ = userService.getUser();
+    }
+
+    logout() {
+        this.userService.logout();
+        this.router.navigate(['']);
     }
 }
 
