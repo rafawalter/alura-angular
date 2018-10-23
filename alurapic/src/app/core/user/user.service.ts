c9cc07af8fdabdc0bde9e14c4de3d97551092cf2
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-10-23 09:27:02 -0300

    02.08 Cabeçalho da aplicação

---
+++ alurapic/src/app/core/user/user.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import * as jwt_decode from 'jwt-decode';
+
+import { TokenService } from '../token/token.service';
+import { User } from './user';
+
+@Injectable({ providedIn: 'root'})
+export class UserService {
+
+    private userSubject = new Subject<User>();
+
+    constructor(
+        private tokenService: TokenService
+    ) {
+        // tslint:disable-next-line:no-unused-expression
+        this.tokenService.hasToken() &&
+            this.decodeAndNotify();
+    }
+
+    setToken(token: string) {
+        this.tokenService.setToken(token);
+        this.decodeAndNotify();
+    }
+
+    getUser() {
+        return this.userSubject.asObservable();
+    }
+
+    private decodeAndNotify(): any {
+        const token = this.tokenService.getToken();
+        const user = jwt_decode(token) as User;
+        this.userSubject.next(user);
+    }
+}
