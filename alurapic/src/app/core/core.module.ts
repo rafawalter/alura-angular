22400617b7010f0fd25ab36c99248c69947a7f68
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-10-23 11:00:45 -0300

    03.10 A diretiva routerLink

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [HeaderComponent],
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        RouterModule
+    ],
     exports: [HeaderComponent]
 })
 export class CoreModule {}
