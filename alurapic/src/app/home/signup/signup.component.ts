276d6b277ecb714352143d15453b7ad55a3a8375
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-10-25 10:36:08 -0300

    04.04 Criando nosso próprio validador

--- alurapic/src/app/home/signup/signup.component.ts
+++ alurapic/src/app/home/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
 
 @Component({
     templateUrl: './signup.component.html'
@@ -25,7 +26,7 @@ export class SignUpComponent implements OnInit {
             ]],
             userName: ['', [
                 Validators.required,
-                Validators.pattern(/^[a-z0-9_\-]+$/),
+                lowerCaseValidator,
                 Validators.minLength(2),
                 Validators.maxLength(30)
             ]],
