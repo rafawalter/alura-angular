cc4f4857fcb46095bb6da2df3c0db44f99260b64
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-11-23 10:09:16 -0200

    03.03 Extraindo parâmetros de rotas

--- alurapic/src/app/photos/photo-details/photo-details.component.ts
+++ alurapic/src/app/photos/photo-details/photo-details.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     templateUrl: './photo-details.component.html'
 })
-export class PhotoDetailsComponent {
+export class PhotoDetailsComponent implements OnInit {
+    constructor(
+        private route: ActivatedRoute
+    ) { }
 
+    ngOnInit(): void {
+        const id = this.route.snapshot.params.photoId;
+        console.log(id);
+    }
 }
