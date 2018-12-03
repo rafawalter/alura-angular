8ff30d14f24b56b7df1331efe86d625548f2d77d
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-12-03 09:20:23 -0200

    04.08 Um problema não esperado

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -19,18 +19,20 @@ export class PhotoListComponent implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private photoService: PhotoService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
-    this.userName = this.activatedRoute.snapshot.params['userName'];
-    this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.activatedRoute.params.subscribe(params => {
+      this.userName = this.activatedRoute.snapshot.params['userName'];
+      this.photos = this.activatedRoute.snapshot.data['photos'];
+    });
   }
 
 
   load() {
     this.photoService
       .listFromUserPaginated(this.userName, ++this.currentPage)
-      .subscribe( photos => {
+      .subscribe(photos => {
         this.filter = '';
         this.photos = this.photos.concat(photos);
         if (!photos.length) { this.hasMore = false; }
