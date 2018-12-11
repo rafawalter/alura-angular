9e68c473a8c42e4eb7986fdcbe0b52452e969fed
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-12-11 10:47:38 -0200

    02.04 Exibindo o progresso

--- alurapic/src/app/core/auth/auth.service.ts
+++ alurapic/src/app/core/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
--- alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -1,9 +1,12 @@
-import { UserService } from './../../core/user/user.service';
-import { AlertService } from './../../shared/components/alerts/alert.service';
-import { PhotoService } from './../photo/photo.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
+import { HttpEvent, HttpEventType, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+
+import { UserService } from './../../core/user/user.service';
+import { AlertService } from './../../shared/components/alerts/alert.service';
+import { PhotoService } from './../photo/photo.service';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -16,6 +19,7 @@ export class PhotoFormComponent implements OnInit {
   photoForm: FormGroup;
   file: File;
   preview: any;
+  percentDone = 0;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -38,10 +42,20 @@ export class PhotoFormComponent implements OnInit {
     const allowComments = this.photoForm.get('allowComments').value;
     this.photoService
       .upload(description, allowComments, this.file)
-      .subscribe(() => {
-        this.alertService.success('Upload complete', true);
+      .pipe(finalize(() => {
         this.router.navigate(['/user', this.userService.getUserName()]);
-      });
+      }))
+      .subscribe(
+        (event: HttpEvent<any>) => {
+          if (event.type === HttpEventType.UploadProgress) {
+            this.percentDone = Math.round(100 * event.loaded / event.total);
+          } else if (event instanceof HttpResponse) {
+            this.alertService.success('Upload complete', true);
+          }
+        }, err => {
+          console.log(err);
+          this.alertService.danger('Upload error!', true);
+        });
   }
 
   handleFile(file: File) {
