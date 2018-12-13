8c99caf118092cc34233897451e8350b5a88218d
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-12-13 10:04:20 -0200

    04.04 Estilo e template

--- alurapic/src/app/shared/components/menu/menu.component.ts
+++ alurapic/src/app/shared/components/menu/menu.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
     // tslint:disable-next-line:component-selector
     selector: 'ap-menu',
-    templateUrl: './menu.component.html'
+    templateUrl: './menu.component.html',
+    styleUrls: ['menu.component.css']
 })
 export class MenuComponent {
 
