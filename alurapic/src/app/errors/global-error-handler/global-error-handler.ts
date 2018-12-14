4f3d7deee8a53b6bfe2dc4633624272108cab180
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-12-14 11:13:50 -0200

    05.06 Exibindo mais informações

--- alurapic/src/app/errors/global-error-handler/global-error-handler.ts
+++ alurapic/src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,17 +1,37 @@
-import { ErrorHandler } from '@angular/core';
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import * as StackTrace from 'stacktrace-js';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 
+@Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
 
+    constructor(
+        private injector: Injector
+    ) { }
+
     handleError(error: any): void {
-        console.log('passei pelo handler');
+
+        const location = this.injector.get(LocationStrategy);
+
+        const url = location instanceof PathLocationStrategy
+            ? location.path()
+            : '';
+
+        const message = error.message
+            ? error.message :
+            error.toString();
+
         StackTrace
             .fromError(error)
             .then(stackFrames => {
                 const stackAsString = stackFrames
                     .map(sf => sf.toString())
                     .join('\n');
+
+                console.log(message);
                 console.log(stackAsString);
+                console.log('o que será enviado para o servidor');
+                console.log({ message, url, stack: stackAsString });
             });
     }
 }
